6f8908cba2f1fcf2d18d834ad40c866c8426d9b1
Claire-Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-11-02 14:17:26 -0400

    fixed volume to five

--- js/script.js
+++ js/script.js
@@ -27,5 +27,5 @@ function myButtonClicked() {
 
   // output
   document.getElementById("volume").innerHTML =
-    "Volume is equal to: " + volume + " cm³"
+    "Volume is equal to: " + volume.toFixed(5) + " cm³"
 }
